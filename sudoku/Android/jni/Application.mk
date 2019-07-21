fe987fd1fe8a89d1a57c200397c9437f46c631ce
zhyingkun <1464959348@qq.com>
2019-07-21 21:38:58 +0800

    Android .mk files

---
+++ sudoku/Android/jni/Application.mk
@@ -0,0 +1,21 @@
+# application project path
+# APP_PROJECT_PATH
+
+APP_MODULES := sudoku
+
+# debug or release
+APP_OPTIM := release
+
+# APP_CFLAGS := 
+# APP_CPPFLAGS := 
+# APP_LDFLAGS := 
+# APP_BUILD_SCRIPT := 
+
+# armeabi-v7a arm64-v8a x86 x86_64 all
+APP_ABI := armeabi-v7a arm64-v8a
+
+APP_PLATFORM := android-19
+
+# APP_STL := 
+# APP_SHORT_COMMANDS := 
+# APP_THIN_ARCHIVE := 
