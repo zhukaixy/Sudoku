201dd173b1f57c963ea4926fcfaa73f13f3b4726
zhyingkun <1464959348@qq.com>
2019-09-04 23:25:27 +0800

    Update C demo with display sudoku

--- demo/for-c/src/for-c.c
+++ demo/for-c/src/for-c.c
@@ -52,6 +52,21 @@ int board[81] = {
 };
 // clang-format on
 
+void display_sudoku(int* sudo) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int index = i * 9 + j;
+            printf("%d ", sudo[index]);
+            if (j == 2 || j == 5)
+                printf(" ");
+        }
+        printf("\n");
+        if (i == 2 || i == 5) {
+            printf("\n");
+        }
+    }
+}
+
 // row and col are in [1, 9]
 static int read_board(int row, int col) {
   return board[(row - 1) * 9 + col - 1];
@@ -65,16 +80,24 @@ int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
   Sudoku* sudo = CreateSudoku(read_board, write_board);
-  char buffer[RESULT_BUFFER_SIZE];
-  MakeResultString(sudo, buffer, RESULT_BUFFER_SIZE);
-  printf("Sudoku Puzzle:\n%s", buffer);
+    
+//  char buffer[RESULT_BUFFER_SIZE];
+//  MakeResultString(sudo, buffer, RESULT_BUFFER_SIZE);
+//  printf("Sudoku Puzzle:\n%s", buffer);
+    printf("Sudoku Puzzle:\n");
+    display_sudoku(board);
+    
   int cnt = GetKnownCount(sudo);
   printf("Sudoku Known Count: %d\n", cnt);
   int num = CalculateSudokuAll(sudo, true, NULL, NULL);
   printf("All Answer Count: %d\n", num);
-  MakeResultString(sudo, buffer, RESULT_BUFFER_SIZE);
-  printf("One of it:\n%s", buffer);
-  int status = VerifySudoku(sudo);
+    
+//  MakeResultString(sudo, buffer, RESULT_BUFFER_SIZE);
+//  printf("One of it:\n%s", buffer);
+    printf("One of it:\n");
+    display_sudoku(board);
+    
+  int status = VerifySudokuBoard(board);
   printf("Verify: %s\n", status ? "True" : "False");
   DestroySudoku(sudo);
   return 0;
