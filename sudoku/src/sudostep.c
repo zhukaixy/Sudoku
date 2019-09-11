237f7803e4a08eee0c090352a82c54dbd300fbc9
zhyingkun <1464959348@qq.com>
2019-09-11 23:04:51 +0800

    New header and Implement

--- sudoku/src/sudostep.c
+++ sudoku/src/sudostep.c
@@ -1,5 +1,8 @@
 #include <sudoku.h>
-#include <sudodef.h>
+#include <sudostep.h>
+#include <methods.h>
 
 void calculate_step_by_step(Sudoku* sudo) {
+  first_remove_posibility(sudo);
+  check_all_posibility(sudo);
 }
---
+++ sudoku/src/sudostep.h
@@ -0,0 +1,8 @@
+#ifndef _SUDO_STEP_H_
+#define _SUDO_STEP_H_
+
+#include <sudodef.h>
+
+void calculate_step_by_step(Sudoku* sudo);
+
+#endif
