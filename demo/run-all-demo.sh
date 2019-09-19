f51019438ae90351964229b0d4344320c2a6b8d3
zhyingkun <1464959348@qq.com>
2019-09-19 22:31:16 +0800

    For demo run in platform

--- demo/run-all-demo.sh
+++ demo/run-all-demo.sh
@@ -1,31 +1,54 @@
 #! /usr/bin/env bash
 
-exitWhileError(){
-    if test $? -ne 0 ; then
-        echo "
+exitWhileError() {
+	if test $? -ne 0; then
+		echo "
         Error Message: $1
         Please Check The Error And Test Again! Exit Now.
         " 1>&2
-        exit
-    fi
+		exit
+	fi
 }
 
 ScriptExe=$0
 while [ -h ${ScriptExe} ]; do
-    ScriptRootDir=$(cd -P $(dirname ${ScriptExe}) && pwd)
-    ScriptExe=$(readlink ${ScriptExe})
-    [[ ${ScriptExe} != /* ]] && ScriptExe=${ScriptRootDir}/${ScriptExe}
+	ScriptRootDir=$(cd -P $(dirname ${ScriptExe}) && pwd)
+	ScriptExe=$(readlink ${ScriptExe})
+	[[ ${ScriptExe} != /* ]] && ScriptExe=${ScriptRootDir}/${ScriptExe}
 done
 ScriptRootDir=$(cd -P $(dirname ${ScriptExe}) && pwd)
 
-AllDemos=(
+AllCommonDemos=(
 	"for-c"
 	"for-cs"
-	"for-go"
 	"for-java"
-);
+)
 
-for demo in ${AllDemos[@]}; do
-	cd ${ScriptRootDir}/${demo} && make && make run
+MacDemos=(
+	"for-go"
+)
+
+LinuxDemos=(
+	"for-go"
+)
+
+ARCH=$(uname -s)
+
+for demo in ${AllCommonDemos[@]}; do
+	cd ${ScriptRootDir}/${demo} && make clean && make && make run
 	exitWhileError "run ${demo} failed"
 done
+
+if [ Y"${ARCH}" == Y"Darwin" ]; then
+	for demo in ${MacDemos[@]}; do
+		cd ${ScriptRootDir}/${demo} && make clean && make && make run
+		exitWhileError "run ${demo} failed"
+	done
+fi
+
+if [ Y"${ARCH}" == Y"Linux" ]; then
+	for demo in ${LinuxDemos[@]}; do
+		cd ${ScriptRootDir}/${demo} && make clean && make && make run
+		exitWhileError "run ${demo} failed"
+	done
+fi
